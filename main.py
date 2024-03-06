2aa77a7f5d0ec3231fe2a59911dc782d973e9ae8
Hoang Bui <hdbui@loyola.edu>
2024-03-06 09:50:53 -0500

    save the results to a file

--- main.py
+++ main.py
@@ -26,10 +26,31 @@ if __name__ == "__main__":
         targeting_image = find_target_pixel(preprocess_image, r_max)
 
         # Process Daughman
-        pupil, iris, circle = Daughman_Algorithm(preprocess_image, targeting_image, r_max)
+        pupil, iris, circle = Daughman_Algorithm(preprocess_image, targeting_image, r_max, image_path)
         print("pupil: ", pupil, " iris: ", iris)
         print("if you want stop, press esc")
-        cv2.imshow("circle", circle)
-        key = cv2.waitKey()
-        if key == 27:
-            break
+        #cv2.imshow("circle", circle)
+        cv2.imwrite("test1.jpg",preprocess_image)
+        image2 = cv2.imread('test1.jpg')
+        mask = np.zeros(image2.shape, dtype=np.uint8)
+        
+        cv2.circle(mask, (iris[0],iris[1]), iris[2], (255,255,255), -1)
+
+        # Bitwise-and for ROI
+        ROI = cv2.bitwise_and(image2, mask)
+
+        # Crop mask and turn background white
+        mask = cv2.cvtColor(mask, cv2.COLOR_BGR2GRAY)
+        x,y,w,h = cv2.boundingRect(mask)
+        result = ROI[y:y+h,x:x+w]
+        mask = mask[y:y+h,x:x+w]
+        result[mask==0] = (255,255,255)
+
+
+        #cv2.imshow('result', result)
+        cv2.imwrite(image_path+"done.jpg",result)
+
+ 
+        #key = cv2.waitKey()
+        #if key == 27:
+        #    break
--- module/image_process.py
+++ module/image_process.py
@@ -49,11 +49,11 @@ def conv_2d(image, x, y, mask):
     return np.sum(masking_image)/count
 
 
-def Daughman_Algorithm(image, preprocess, r_max):
+def Daughman_Algorithm(image, preprocess, r_max, image_path):
     r_min = 15
     height, width = image.shape
     max_value, max_x, max_y, max_r = 0, 0, 0, 0
-    daughman_values = np.zeros((height-r_max*2,width-r_max*2, r_max - r_min + 1), np.float)
+    daughman_values = np.zeros((height-r_max*2,width-r_max*2, r_max - r_min + 1), float)
     for r in range(r_min, r_max+1):
         print("process..... ", r)
         mask = np.zeros((r*2,r*2), np.uint8)
@@ -90,4 +90,39 @@ def Daughman_Algorithm(image, preprocess, r_max):
 
     circle_image = cv2.circle(circle, (max_x, max_y), max_r, 255, 1)
     iris = (max_x, max_y, max_r)
+
+    #height,width = image.shape
+    #mask = np.zeros((height,width), np.uint8)
+
+    image2 = cv2.imread(image_path)
+
+
+    mask = np.zeros(image2.shape, dtype=np.uint8)
+
+    cv2.circle(mask, (max_x,max_y), max_r, (255,255,255), -1)
+    ROI = cv2.bitwise_and(image2, mask)
+
+    mask = cv2.cvtColor(mask, cv2.COLOR_BGR2GRAY)
+    x,y,w,h = cv2.boundingRect(mask)
+    result = ROI[y:y+h,x:x+w]
+    mask = mask[y:y+h,x:x+w]
+    result[mask==0] = (255,255,255)
+
+
+
+    cv2.imwrite('test.jpg', result)
+
+    #mask1 = np.zeros_like(image)
+    #mask1 = cv2.circle(mask1, (max_x,max_y), 0, (255,255,255), -1)
+    #mask2 = np.zeros_like(image)
+    #mask2 = cv2.circle(mask2, (max_x,max_y), max_r, (255,255,255), -1)
+
+    #mask = cv2.subtract(mask2, mask1)
+
+# put mask into alpha channel of input
+    #result = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY) #cv2.COLOR_BGR2BGRA)
+    #result[:, :, 3] = mask[:,:,0]
+    
+    #cv2.imwrite('test.jpg', result)
+
     return pupil, iris, circle_image
